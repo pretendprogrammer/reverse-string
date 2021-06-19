e35e590eabf1b77d495649622a610f6ee0a5239e
Yosef <shmuli5770@gmail.com>
2021-06-19 16:16:58 -0400

    commit tree 85e534dd81f40ef296939f3a44987da2dfc60afd

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,8 +1,20 @@
 function reverseString(str) {
-  // type your code here
+  // Create an empty array in which to house the final value to be returned by the function
+  let output = [];
+  // Iterate over an array-representation of the input string
+  Array.from(str).forEach((char) => {
+    // Add it to the beginning of our output array
+    output.unshift(char);
+  });
+  // Return final output value after iteration is complete
+  return output.join("");
 }
 
 if (require.main === module) {
+  console.log("Expecting: 'yellow'");
+  console.log("=>", reverseString("wolley"));
+  console.log("Expecting: 'azzip'");
+  console.log("=>", reverseString("pizza"));
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
@@ -14,6 +26,3 @@ if (require.main === module) {
 }
 
 module.exports = reverseString;
-
-// Please add your pseudocode to this file
-// And a written explanation of your solution
